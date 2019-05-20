60f0a8804102e16aa96032e15a2a4a82f7a5acca
d.sotirakis <d.sotirakis@ocado.com>
2019-05-20 13:15:05 +0100

    Removed unnecessary files

--- target/classes/data-h2.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Users (NAME, SURNAME, EMAILADDRESS) VALUES ('name1', 'surname1','name1@gmail.com');
-INSERT INTO Users (NAME, SURNAME, EMAILADDRESS) VALUES ('name2', 'surname2','name2@gmail.com');
-INSERT INTO Users (NAME, SURNAME, EMAILADDRESS) VALUES ('name3', 'surname3','name3@gmail.com');
-
-INSERT INTO Accounts (USERNAME, PASSWORD, BALANCE) VALUES ('name1', 'password1','10000.00');
-INSERT INTO Accounts (USERNAME, PASSWORD, BALANCE) VALUES ('name2', 'password2','20000.00');
-INSERT INTO Accounts (USERNAME, PASSWORD, BALANCE) VALUES ('name3', 'password3','30000.00');
-
